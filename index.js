f8e3b14c727e49383cb52426b33ad20ea3202e0c
Lisa Mao <liisamao@gmail.com>
2018-08-13 16:08:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $("#joke").on("click", function() {
+    alert("You've been clicked!");
+  });
+  //document.getElementById("joke").addEventListener("click", function(){ alert("You've been clicked!"); });
+})
+
